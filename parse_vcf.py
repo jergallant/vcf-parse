1b1ae4458d592e731cece636be84ecf408e6c2dd
Jeremy Gallant <jergallant@gmail.com>
2020-03-05 21:20:48 +1100

    Add some extra comments

--- parse_vcf.py
+++ parse_vcf.py
@@ -10,6 +10,7 @@ args = parser.parse_args()
 in_file = args.input_file
 out_file = args.output if args.output else None
 
+# Wrap the file open commands in try-except to avoid throwing stack-traces at the user
 try:
     input_fo = open(args.input_file, 'r')
 except FileNotFoundError:
@@ -21,7 +22,7 @@ if out_file:
     except Exception as e:
         sys.exit("Could not open the specified output file for writing.\nError message: %s" %e)
 
-# Map the column names to list indices
+# Map the fixed, mandatory column names to positions
 # These column positions are fixed by the spec, so we shouldn't need to build this dynamically from the header row
 column_map = {
     "#CHROM": 0,
@@ -41,28 +42,36 @@ while True:
         # We have reached EOF. (Empty lines will be '\n')
         break
 
+    # clean up the newline at the end of the line
     line = line.rstrip('\n')
+
+    # Skip metadata lines
     if line.startswith('##'):
-        # This is a metadata line
         continue
-
+    
+    # Skip the header line
     if line.startswith('#CHROM'):
-        # This is the header line
         continue
 
+    # Split data line into list using tab delimiter
     entries = line.split('\t')
+    # Select the relevant values from the row
     chrom = entries[column_map['#CHROM']]
     pos = entries[column_map['POS']]
     ref = entries[column_map['REF']]
     alt = entries[column_map['ALT']]
+    
+    # Make the output string
     line_out = f'chr{chrom}:{pos}{ref}>{alt}'
     
+    # Write output to file, or print out if no output file is specified
     if out_file:
         output_fo.write(line_out)
         output_fo.write('\n')
     else:
         print(line_out)
 
+# Close those file handles
 input_fo.close()
 if out_file:
     output_fo.close()
